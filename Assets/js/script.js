770c3435efb891c48dceea0b3a5683138dcd7c97
JANAKALIDASAN T <104510427+Janakalidasan@users.noreply.github.com>
2023-04-28 16:36:20 +0530

    index

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -24,6 +24,16 @@ fetch("./footer.html")
     .then(data => {
         document.querySelector("footer").innerHTML = data;
     });
+
 // new
-
-
+const myButton = document.getElementById('but');
+const inr1 = document.getElementById('inr-1');
+const inr = document.getElementById('inr');
+const usd1 = document.getElementById('usd-1');
+const usd = document.getElementById('usd');
+myButton.addEventListener('click', () => {
+  inr1.style.display = inr1.style.display === 'none' ? 'block' : 'none';
+  inr.style.display = inr.style.display === 'block' ? 'none' : 'block';
+  usd1.style.display = usd1.style.display === 'none' ? 'block' : 'none';
+  usd.style.display = usd.style.display === 'block' ? 'none' : 'block';
+});
