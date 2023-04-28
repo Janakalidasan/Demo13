39d5e1d627262ba3806302b85ed16c6853d429da
JANAKALIDASAN T <104510427+Janakalidasan@users.noreply.github.com>
2023-04-28 17:43:22 +0530

    account

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -31,9 +31,13 @@ const inr1 = document.getElementById('inr-1');
 const inr = document.getElementById('inr');
 const usd1 = document.getElementById('usd-1');
 const usd = document.getElementById('usd');
+const chage = document.getElementById('chage');
+const chage1 = document.getElementById('chage-1');
 myButton.addEventListener('click', () => {
   inr1.style.display = inr1.style.display === 'none' ? 'block' : 'none';
   inr.style.display = inr.style.display === 'block' ? 'none' : 'block';
   usd1.style.display = usd1.style.display === 'none' ? 'block' : 'none';
   usd.style.display = usd.style.display === 'block' ? 'none' : 'block';
+  chage1.style.display = chage1.style.display === 'none' ? 'block' : 'none';
+  chage.style.display = chage.style.display === 'block' ? 'none' : 'block';
 });
